b4881d52e63bd1b050b30c8060862418ab2e268f
JMW-168 <chengjinming@MacBook-Mike.local>
2025-05-26 13:43:54 +0800

    占位符號實作

--- cmd/demo/funcdemo/main/main.go
+++ cmd/demo/funcdemo/main/main.go
@@ -11,4 +11,9 @@ func main() {
 	b := 15
 	res1, res2 := calc.GetSumAndSub(a, b)
 	fmt.Printf("a 和 b 的總和是 %d \na 和 b 的差是 %d\n", res1, res2)
+
+	//如果希望忽略某個返回值，使用 _ 符號表示占位忽略
+
+	_, res3 := calc.GetSumAndSub(1, 2)
+	fmt.Println("res3", res3)
 }
