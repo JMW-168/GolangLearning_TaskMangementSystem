0fdbc29964b3520876ee809ee33c27df7cfe1eff
JMW-168 <chengjinming@MacBook-Mike.local>
2025-05-27 14:58:04 +0800

    指針傳遞array

--- cmd/demo/chapter07/arraydetail/main.go
+++ cmd/demo/chapter07/arraydetail/main.go
@@ -8,8 +8,15 @@ func test01(arr [3]int) {
 	arr[0] = 88
 }
 
+func test02(arr *[3]int) {
+	(*arr)[0] = 88 //重要
+}
+
 func main() {
+	// arr := [3]int{11, 22, 33}
+	// test01(arr)
+	// fmt.Println("main arr =", arr)
 	arr := [3]int{11, 22, 33}
-	test01(arr)
+	test02(&arr)
 	fmt.Println("main arr =", arr)
 }
