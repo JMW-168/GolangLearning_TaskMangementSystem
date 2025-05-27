a276fb99f2088cff64e7ca9f638f13e03842aec8
JMW-168 <chengjinming@MacBook-Mike.local>
2025-05-27 12:26:07 +0800

    自定義錯誤處理 實作

--- cmd/demo/errrorhandle/errorhandle/main.go
+++ cmd/demo/errrorhandle/errorhandle/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 	test()
+	test02()
 	fmt.Println("main()下面的code")
 
 }
@@ -22,5 +24,26 @@ func test() {
 	num2 := 0
 	res := num1 / num2
 	fmt.Println("res", res)
+
+}
+
+func readConf(name string) (err error) {
+	if name == "config.ini" {
+		//讀取
+		return nil
+	} else {
+		//返回一個自定義錯誤
+		return errors.New("讀取文件錯誤~")
+	}
+}
+
+func test02() {
+
+	err := readConf("config2.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("test02 繼續執行")
 
 }
