87201533900aa3e8fa53ffa7ec5a945d8e17e69a
JMW-168 <chengjinming@MacBook-Mike.local>
2025-05-27 12:04:58 +0800

    錯誤處理實作

---
+++ cmd/demo/errrorhandle/errorhandle/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	test()
+	fmt.Println("main()下面的code")
+
+}
+
+func test() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("err=", err)
+		}
+
+	}()
+	num1 := 10
+	num2 := 0
+	res := num1 / num2
+	fmt.Println("res", res)
+
+}
