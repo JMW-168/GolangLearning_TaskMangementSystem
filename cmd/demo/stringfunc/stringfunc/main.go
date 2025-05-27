281de08c3924c24940dd6a0cfce8646eb10cf06c
JMW-168 <chengjinming@MacBook-Mike.local>
2025-05-27 10:52:54 +0800

    Split 字符串分割

--- cmd/demo/stringfunc/stringfunc/main.go
+++ cmd/demo/stringfunc/stringfunc/main.go
@@ -1,19 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
+	/*
+		str := "hello北七"
+		//統計字符串的長度，中文一字三個字節，英數一字一個字節
+		fmt.Println("str lens=", len(str))
 
-	str := "hello北七"
-	//統計字符串的長度，中文一字三個字節，英數一字一個字節
-	fmt.Println("str lens=", len(str))
+		str2 := "hello西安"
+		//字符串遍歷，因為中文三字節，會被分開有問題，使用 r:=[]rune(str)
+		r := []rune(str2)
 
-	str2 := "hello西安"
-	//字符串遍歷，因為中文三字節，會被分開有問題，使用 r:=[]rune(str)
-	r := []rune(str2)
-
-	for i := 0; i < len(r); i++ {
-		fmt.Printf("字符=%c\n", r[i])
+		for i := 0; i < len(r); i++ {
+			fmt.Printf("字符=%c\n", r[i])
+		}
+	`	*/
+	strArr := strings.Split("hello,world,ok", ",")
+	for i := 0; i < len(strArr); i++ {
+		fmt.Printf("str[%v]=%v\n", i, strArr[i])
 	}
-
+	fmt.Printf("strArr = %v\n", strArr)
 }
